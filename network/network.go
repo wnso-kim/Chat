b0766fbf8b6441944fac401f9dde957c9aa77479
wnso-kim <wnso.kim@gmail.com>
2024-04-14 12:02:51 +0900

    feat > Use 미들웨어, Cors 설정

--- network/network.go
+++ network/network.go
@@ -3,6 +3,7 @@ package network
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,16 @@ type Network struct {
 
 func NewServer() *Network {
 	n := &Network{engin: gin.New()}
+
+	n.engin.Use(gin.Logger())         // API 로거
+	n.engin.Use(gin.Recovery())       // 패닝이 일어나 특정 로직이 잘못돼서 서버가 다운 되는 경우 다시 서버를 올려주는 역할
+	n.engin.Use(cors.New(cors.Config{ // CORS 설정
+		AllowWebSockets:  true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
 
 	return n
 }
