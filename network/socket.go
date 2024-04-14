f243de8e1745be1513f6396398c155d895d1d3d6
wnso-kim <wnso.kim@gmail.com>
2024-04-14 19:00:27 +0900

    fix > 동시성 처리 에러 핸들러 추가
    - close된 채널에서 소켓을 사용하는 경우 에러 발생!
    - 해당 에러를 처리하기 위해 추가

--- network/socket.go
+++ network/socket.go
@@ -56,7 +56,11 @@ func (c *client) Read() {
 		var msg *message
 		err := c.Socket.ReadJSON(&msg)
 		if err != nil {
-			panic(err)
+			if !websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				break
+			} else {
+				panic(err)
+			}
 		} else {
 			msg.Time = time.Now().Unix()
 			msg.Name = c.Name
